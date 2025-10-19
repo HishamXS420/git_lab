8d4f28ff1b6efd6348286ad0f84b8965bb73c4cd
Zaif96 <clashofclans.hisham@gmail.com>
2025-10-19 08:13:03 +0600

    Line changed by zaif

--- git.cpp
+++ git.cpp
@@ -8,7 +8,7 @@ cout << "Let's gets started" << endl;
 
 cout << " This is our first lab for CSE-3206" << endl;  // It's Zaif reporting
 
-cout << "You are welcome to the project Zaif96!" << endl; // Hisham Reporting (Owner)
+cout << "You are welcome to the project Zaif96!" << endl; 
 
 return 0 ;
 }
