730ff83946c1629f627146f9d75d5a8a03c64790
Zaif96 <clashofclans.hisham@gmail.com>
2025-10-19 07:33:42 +0600

    I am collaborating in this project - Zaif96

--- git.cpp
+++ git.cpp
@@ -6,5 +6,7 @@ int main(){
 cout << "This is my first project at git" << endl;
 cout << "Let's gets started" << endl;
 
+cout << " This is our first lab for CSE-3206" << endl;  // It's Zaif reporting
+
 return 0 ;
 }
