1f8a8f7371c0a49808dfda6a8f78e949f2da7bc9
Zaif96 <clashofclans.hisham@gmail.com>
2025-10-19 08:27:07 +0600

    Final push of the session

--- git.cpp
+++ git.cpp
@@ -8,7 +8,9 @@ cout << "Let's gets started" << endl;
 
 cout << " This is our first lab for CSE-3206" << endl;  // It's Zaif reporting
 
-cout << "You are welcome to the project Zaif96!" << endl; // 
+cout << "You are welcome to the project Zaif96!" << endl; 
+
+cout << "This is the end of this session" << endl; // Hisham marked
 
 return 0 ;
 }
